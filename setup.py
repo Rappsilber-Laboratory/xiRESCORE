91c6c037504964fb58aca6c3b4b6161d098f8f3e
Falk B. Schimweg <git@falk.schimweg.de>
2025-07-09 12:54:52 +0200

    lower version of arrow

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ requirements = [
     "psycopg2>=2.9",
     "python-logging-loki",
     "fastparquet>=2022.11.0",
-    "pyarrow",
+    "pyarrow<=4",
     "scipy>=1.0.1",
     "setuptools~=71.1.0",
     "xiutilities~=1.2.3",
