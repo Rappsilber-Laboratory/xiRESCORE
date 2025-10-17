e2af8a01cb39e81d020fe1cd3c3bf6cc9d878e99
Falk B. Schimweg <git@falk.schimweg.de>
2025-10-17 15:10:09 +0200

    Avoid empty feature prefix

--- xirescore/feature_extracting.py
+++ xirescore/feature_extracting.py
@@ -10,7 +10,7 @@ def get_features(df: pl.DataFrame, options: dict, filter_nan=True):
     features_const = options['input']['columns']['features']
     feat_prefix = options['input']['columns']['feature_prefix']
     features_prefixes = []
-    if features_prefixes is not None and features_prefixes != "":
+    if feat_prefix is not None and feat_prefix != "":
         features_prefixes = [
             c for c in df.columns if str(c).startswith(feat_prefix)
         ]
